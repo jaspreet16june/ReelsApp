f05c38f417d3afbefe350df34c134055440476bd
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-08-17 16:25:25 +0530

    ReelsApp

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app"
+const firebaseConfig = {
+    apiKey: "AIzaSyBInpYf2APB5ARhGINOjy5azErYkBA4b-Y",
+    authDomain: "reels-app-25c24.firebaseapp.com",
+    projectId: "reels-app-25c24",
+    storageBucket: "reels-app-25c24.appspot.com",
+    messagingSenderId: "884871511428",
+    appId: "1:884871511428:web:d760c9668df683a2e971d1"
+  };
+
+  firebase.initializeApp(firebaseConfig);
